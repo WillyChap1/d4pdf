ce08296cb618eaf28fea7f5bbd5d96a79ad135e2
Will Chapman <will.eric.chapman@gmail.com>
2018-02-27 07:57:57 -0800

    Adding Formating files to format by year

---
+++ Qhum/Qformat.m
@@ -0,0 +1,35 @@
+%prep IVT ensembles 
+clear all
+cd /Users/wchapman/SIO/research/100Amip/d4pdf/Qlev/lev
+s = dir('Q*.nc');
+
+for ii = 1952:2011 
+    disp(ii)
+    tic
+    for jj = 1:length(s)
+        
+        filename = s(jj).name;
+        time = ncread(filename,'time');
+        ivt = ncread(filename,'ivtt');
+        lat =ncread(filename,'lat');
+        lon =ncread(filename,'lon');
+
+        datestart = '01-Jan-1951 00:00:00';
+        formatin = 'dd-MMM-yyyy HH:mm:ss';
+        date1 = datetime(datestart,'InputFormat',formatin);
+
+        timevector = date1 + minutes(time);
+        dateMat = datevec(timevector);
+    
+    
+        indexget = find(dateMat(:,1)==ii);
+    
+        IVTtot(:,:,jj,:) = ivt(:,:,indexget);
+        timer = timevector(indexget);
+
+    end
+    toc
+    file2save = strcat(filename(1:4),'______',num2str(ii),'0101_',num2str(ii),'1231.mat');
+    save(file2save,'lon','lat','timer','IVTtot')
+    
+end 
